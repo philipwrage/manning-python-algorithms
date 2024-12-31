bd8cf49300095789fd3f3d786e4cf9ab5338fac4
Philip Wrage <philipwrage@users.noreply.github.com>
2024-12-31 15:40:43 -0600

    Implement basic BinaryNode

--- src/trees/binary_tree.py
+++ src/trees/binary_tree.py
@@ -1,2 +1,18 @@
 class BinaryNode:
-    pass
+    def __init__(self, value):
+        self.value = value
+        self.left_child = None
+        self.right_child = None
+
+    def add_left(self, node):
+        self.left_child = node
+        return self
+
+    def add_right(self, node):
+        self.right_child = node
+        return self
+
+    def __str__(self):
+        left_child_value = self.left_child.value if self.left_child else self.left_child
+        right_child_value = self.right_child.value if self.right_child else self.right_child
+        return f"{self.value}: {left_child_value} {right_child_value}"
